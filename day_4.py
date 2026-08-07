e0e3e1620a408353ca2f94ffeadf990f861e5f87
cls <nehadubey09908@gmail.com>
2026-08-07 19:31:02 +0530

    day_4

---
+++ day_4.py
@@ -0,0 +1,128 @@
+# 5W things of Python :- 
+
+# What 
+# Where 
+# When 
+# Why 
+# Who
+# _______________________________________
+
+
+# 1. WHat is Python ?
+
+# Python is a High-level Language 
+# Python is a High- level interpreted language
+
+# _______________________________________
+
+# Types of Programming languages
+
+# a.Interpreted Language
+
+# It runs the code line by line .
+# It takes more time to run.
+
+# Example:
+
+# Python
+
+# _______________________________________
+
+# b. Compiled Language :- 
+
+# It runs the entire code at a time.
+# It takes less time to run.
+
+# Example :
+# Java
+# C 
+# C++ 
+# _______________________________________
+
+
+# c.Hybrid Language :-
+
+# It is the combination of Compiled and Interpreted Language
+
+# Example: 
+
+# JavaScript 
+# _______________________________________
+
+
+# 2.Who, When, Where
+
+# Guido Van Rossum developed Python in Feb 1991 in the Netherlands.
+
+# _______________________________________
+# 3. Why do we need to use Python?
+
+# It is easy to understand
+# It is  easy to read and write
+# It is easy to use 
+# _______________________________________
+
+
+# C Language Example 
+
+# include <stdio.h>
+
+# int main()
+# {
+#     printf("ROhan");
+
+#     return 0;
+# }
+# _______________________________________
+
+# C++ Language Example
+
+# include <iostream>
+
+# int main()
+# {
+#     std::cout<<"Rohan";
+
+#     return 0;
+# }
+
+# _______________________________________
+
+# Python Example
+
+# print("Rohan")
+
+# _______________________________________
+
+# Java Example
+
+# public class Main
+# {
+# 	public static void main(String[] args) {
+# 		System.out.println("Rohan");
+# 	}
+# }
+# _______________________________________
+
+# JavaScript Example
+
+# console.log("ROhan")
+# _______________________________________
+
+# Python Ecosystem
+
+# Python has a large ecosystem
+
+#  Libraries and frameworks 
+
+# Django
+# FastAPI
+# FLask
+# NumPy
+# Pandas 
+# _______________________________________
+
+# Task
+
+# 1. Explore Git and GitHub 
+# 2. What is Variable
