31735e7632bda0dcf38183d014fdaabd6f9b5115
cls <nehadubey09908@gmail.com>
2026-08-07 19:13:02 +0530

    day_3

---
+++ day_3.py
@@ -0,0 +1,87 @@
+# 3. Desktop Application :-
+
+# it does not require any internet connection.
+# it is developed for Desktop only. 
+
+# Examples : 
+
+# MS WORD, 
+# NOTEPAD, 
+# EXCEL , 
+# POWER POINT etc.
+
+# _______________________________________
+
+# Application Architecture 
+
+# Parts of application
+
+# Frontend 
+# Backend 
+# Database
+
+# _______________________________________
+
+# 1. Frontend 
+
+
+# it deals with User interaction (UI).
+# It is User Accessible.
+
+
+# Technologies:
+
+# 1. HTML → it is used to build the body of any webpage.
+# 2. CSS → it is used to apply styling in any webpage.
+# 3. Bootstrap → it's the combination of html & css, it is library of CSS
+# 4. JavaScript → it is a Hybrid Language. 
+# 5. React →  it is Js library
+# _______________________________________
+
+
+
+# 2. Backend :- 
+
+
+# It is the owner accessible parts of any application
+
+# Technologies:-
+
+# Python Library 
+
+# Django
+# FastAPI
+# FLask
+# NumPy 
+# Pandas
+
+# Java Library
+ 
+# SpringBoot
+
+# javascript Library 
+
+# Node.js
+# Express.js 
+# Next.js
+
+# PHP Library 
+
+# Laravel
+# Symfony
+
+# _______________________________________
+
+# 3. Database:- 
+
+
+# It is a platform where we store our data in an organized form
+
+# Examples:
+
+# MYSQL,
+# PostgreSQL, 
+# MongoDB etc.
+
+_______________________________________
+
