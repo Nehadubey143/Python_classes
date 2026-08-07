9c7929b1670cdc45c0927e3ee6d86741d57b5bda
cls <nehadubey09908@gmail.com>
2026-08-07 16:12:13 +0530

    first commit

---
+++ Day_1.py
@@ -0,0 +1,109 @@
+# Day : 1
+# What is Programming Language ?
+#  it is used to create communication between a computer and a person.
+# _______________________________________
+
+# Types of Programming Language:-
+
+# 1. HIgh Level Language :-
+
+# it is software related.
+#  it is used to build any website, software, AI tools, gaming application, in Machine Learning, data science etc.
+#  Example: java, python, javascript, c++ ,PHP etc.
+# _______________________________________
+
+# 2. Low Level Language :-
+
+# it is Hardware related.
+# it is used in washing machines, induction , Keyboard, Mouse , Joystick etc.
+# Example : Machine language, assembly language etc.
+# _______________________________________
+
+# Hardware and Software
+
+# Hardware → it is tangible/touchable.
+# Software → it is not tangible
+
+# _______________________________________
+# Notes:
+# There are 2 types of Language on the basis of compiler :-
+
+# Compiled Language :- 
+# Interpreted Language:-
+
+# _______________________________________
+
+# Application :- 
+
+# An application is a type of software that is designed to perform some specific task
+
+# _______________________________________
+
+# Types of Application:-
+
+# Web Application 
+# Mobile Application 
+# Desktop Application 
+
+# _______________________________________
+
+# 1. Web Application :-
+# it runs inside any browser  ( chrome, Microsoft edge, firefox)
+# it require any internet connection 
+# it is platform independent.
+
+# NOTE:
+# Platform = Operating system
+
+# Examples
+
+# https://www.flipkart.com 
+# https://www.youtube.com
+
+# _______________________________________
+ 
+
+# 2. Mobile Application :-
+
+# it runs inside any mobile device.
+# _______________________________________
+
+# Types of Mobile Application 
+
+# Mobile Web application :-
+
+# it runs as a website inside mobile devices also.
+# it runs inside any browser  ( chrome, Microsoft edge, firefox)
+# it require any internet connection 
+# it is platform independent
+
+# Examples:
+
+# https://www.flipkart.com
+# https://www.instagram.com
+# _______________________________________
+
+# b) Native Application :-
+
+# it is developed for any specific platform.
+# it is platform dependent.
+
+# Examples:
+
+# Appstore, 
+# Playstore, 
+# apple music, etc.
+# _______________________________________
+
+
+# c) Hybrid Application :-
+
+# it's the combination of Native and Mobile web applications.
+# it runs inside any browser , runs as an app, installed from Appstore, or playstore. 
+
+# Examples:
+
+# Youtube,
+#  Instagram,
+#  Flipkart
+#  Amazon etc.
