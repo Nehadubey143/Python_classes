dc30fa6d39bf1fb7594933cc2ddcb021e9e1534d
cls <nehadubey09908@gmail.com>
2026-08-08 22:34:40 +0530

    day_6

---
+++ day_6.py
@@ -0,0 +1,63 @@
+# Git download locally  , and setup following this video:
+# https://youtu.be/t2-l3WvWvqg?si=onv6-lvbbFrqGLSs
+
+# Create GitHub Account , and create Username and password ( credential)
+# Open CMD , and check for version → git -- version
+# Configure Git 
+
+# git config --global user.name "asutoshyadav846"
+# git config --global user.email "asutoshyadav846@gmail.com"
+
+# Check configuration
+
+# git config --global user.email
+# git config --global user.name
+# _______________________________________
+
+# Create a new folder on desktop
+ 
+# Delete path and type → CMD 
+# Press Enter 
+# Type some code 
+
+# Now this folder can open in VS code 
+# _______________________________________
+
+# Git Commands 
+
+# Step: 1
+# Firstly we have to create a file , and write some lines of code
+ 
+# Step 2 
+# Click on NEW terminal
+
+# Step 2.1 => open github and create new REPOSETRIES , and provide a name , and follow the commands 
+
+# Step 3 
+# git init  (“connect folder with github”)
+
+# Step 4 
+# git add . ( “add all files to commit ”) 
+# (“for single file commit ,we use git add nav.html”)
+
+# Step 5
+# git status (“we can see status like which files are committed on git”)
+
+# Step 6
+# git commit -m “first commit”
+
+# Step 7 
+# rest of commands 
+# _______________________________________
+
+# How to add a new  file in our existing repo?
+
+# Step: 1
+# git add “file name”  OR git add .
+# Step 2 
+# git commit -m “your message”
+# Step 3 
+# git push -u origin main
+
+# _______________________________________
+# # 
