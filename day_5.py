5f5e0210afc34ddddef7c82265e06e6f8f5fde5a
cls <nehadubey09908@gmail.com>
2026-08-08 16:05:22 +0530

    dayy_5

---
+++ day_5.py
@@ -0,0 +1,111 @@
+
+
+# 1. Arithmetic Operator 
+# ( +,-,*,%,/,//,**)
+
+
+# Data Types:- 
+
+# Integer : 11,22,3,4,5..........
+# Float : 3.33,5.5, 77.9 ..............
+# string : "Vivek", "Rishi", "Ashutosh"
+# Boolen : TRUE , FALSE 
+# _______________________________________
+
+
+
+ a = 10 
+ b = 3 
+
+
+ print(a + b) # 13
+ print(a - b ) # 7
+ print(a * b ) # 30
+# _______________________________________
+
+# Modulous ("%")
+
+# it gives us the remainder part after performing the division operation.
+
+ a = 10 
+ b = 3 
+ print("output: ", a % b ) # 1
+# _______________________________________
+
+# Division ("/")
+
+# it gives us the Quotient part after performing the division operation.
+# it gives us float data type value .
+
+ a = 10 
+ b = 3 
+ print("output: ", a / b ) # 3.333333
+# _______________________________________
+
+# Floor Division ("//")
+
+# It also gives us QUOTIENT parts after performing the division operation.
+# it gives us integer data type  value 
+
+a = 10 
+b = 3 
+print("output: ", a // b ) # 3
+# _______________________________________
+
+# Exponentiation ("**"):- 
+
+# Used to calculate power of a number 
+# 3 pe power 2 => 9
+
+a = 5
+b = 2 
+print(a ** b )   5 pe power 2 => #25
+# _______________________________________
+
+# 3.Logical Operator :-
+
+# Type of Logical Operators
+
+# and 
+# or 
+# not
+# _______________________________________
+
+# AND Operator
+# If both cases are TRUE it gives TRUE , otherwise it gives FALSE.
+
+# Examples:
+
+a = True
+b = False 
+c = True 
+print( a and b ) # False 
+print(a and c ) # True 
+print(b and c) # False 
+# _______________________________________
+# OR Operator
+
+# If both case are FALSE it gives FALSE , otherwise it gives TRUE
+
+# Examples 
+a = True
+b = False 
+c = True 
+print( a or b ) # True
+print(a or c ) #True
+print(b or c) # True 
+# _______________________________________
+
+# Not Operator
+
+# It gives us reverse value.
+ 
+# Example:-1
+a = True 
+print(not a ) # False 
+
+# Example:-2
+a = 10 
+b = 20 
+print(a == b) # False 
+# _______________________________________
